f304f2cd36a6d8d7f8a6d04c64f2773f2ab8f1df
ahropak <ahropak@nvidia.com>
2017-01-13 20:39:15 -0800

    Add typed placeholder to store typed class instances into

--- ioc.hpp
+++ ioc.hpp
@@ -15,8 +15,17 @@ namespace org {
 					/// Generic placeholder to store typed content in to be used inside the same collection
 					class UntypedPlaceholder {
 					public:
-						virtual void __RTTI__() { /* NOP */ }
-						virtual ~GenericFolder() { /* NOP */ }
+						virtual void __RTTI__() { /* Trick the compiler to generate vtable and enable RTTI placeholder */ }
+						virtual ~UntypedPlaceholder() { /* NC */ }
+					};
+					
+					///Typed placeholder to store creation and instance of typed instances
+					template <typename T>
+					class Placeholder : public UntypedPlaceholder {
+					public:
+						std::shared_ptr<T> _instance; /* to store typed class instance */
+						virtual void __RTTI__() { /* Matches its parent's purpose */ }
+						virtual ~Placeholder() { /* NC */ }
 					};
 				};
 			}
