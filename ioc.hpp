77ad676fa5c09cff282775fec91f22518ef896d6
ahropak <ahropak@nvidia.com>
2017-01-13 20:21:07 -0800

    Add untyped folder to store typed content inside a typed container

--- ioc.hpp
+++ ioc.hpp
@@ -6,16 +6,18 @@
  * @author unixdev0
  * @version 0.0
  */
-namespace org
-{
-	namespace unixdev
-	{
-		namespace ioc 
-		{
-			namespace framework 
-			{
+namespace org {
+	namespace unixdev {
+		namespace ioc {
+			namespace framework {
 				class Ioc {
-
+				private:
+					/// Generic placeholder to store typed content in to be used inside the same collection
+					class UntypedPlaceholder {
+					public:
+						virtual void __RTTI__() { /* NOP */ }
+						virtual ~GenericFolder() { /* NOP */ }
+					};
 				};
 			}
 		}
