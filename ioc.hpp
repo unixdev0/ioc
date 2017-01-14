68cb3b47e85d02ea6c7b25e451dddbeb741546bf
ahropak <ahropak@nvidia.com>
2017-01-13 20:16:42 -0800

    Add initial implementation skeleton

---
+++ ioc.hpp
@@ -0,0 +1,24 @@
+
+#pragma once
+
+/**
+ * Generic Inversion Of Control Container
+ * @author unixdev0
+ * @version 0.0
+ */
+namespace org
+{
+	namespace unixdev
+	{
+		namespace ioc 
+		{
+			namespace framework 
+			{
+				class Ioc {
+
+				};
+			}
+		}
+	}
+}
+
